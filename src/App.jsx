3a3ce61343be099f141ecc55e5e3da3120c9b0d4
GabryelBoer <107651799+GabryelBoer@users.noreply.github.com>
2023-02-17 22:03:27 -0300

    Atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import {Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h2>Movies Lib</h2>
       <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { useState, useEffect } from 'react';
+import MovieCard from '../components/MovieCard';
 
-export default Home
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(`${moviesURL}top_rated?${apiKey}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTopMovies(data.results);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
